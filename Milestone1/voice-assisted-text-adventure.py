db5b5900205a6721045a5ecce3e8609b866cc8b8
Patrick Haynes <patrick.haynes@unomaha.edu>
2023-10-18 19:15:00 -0500

    Revert current game branching path options

--- Milestone1/voice-assisted-text-adventure.py
+++ Milestone1/voice-assisted-text-adventure.py
@@ -3,51 +3,20 @@ import speech_recognition as sr
 # Define game's branching paths
 game = {
     "start": {
-        "text": "You find yourself at the entrance of a dimly lit cave. Do you want to 'enter' deeper into the cave or 'climb' upwards?",
-        "options": {"enter": "river", "climb": "campsite"}
+        "text": "You are in a dark room. Do you want to go 'left' or 'right'?",
+        "options": {"left": "left_room", "right": "right_room"}
     },
-    "river": {
-        "text": "You stumble upon an underground river. Do you want to 'swim', 'boat', or 'back'?",
-        "options": {"swim": "cavern", "boat": "door", "back": "start"}
+    "left_room": {
+        "text": "You find a treasure chest! Do you want to 'open' it or 'leave'?",
+        "options": {"open": "treasure", "leave": "start"}
     },
-    "campsite": {
-        "text": "You discover an abandoned campsite. Do you want to 'search', 'ascend', or 'descend'?",
-        "options": {"search": "library", "ascend": "chamber", "descend": "start"}
-    },
-    "cavern": {
-        "text": "You're in a cavern filled with glowing crystals. Do you want to 'collect', 'look' around, or 'return'?",
-        "options": {"collect": "lava", "look": "dragon", "return": "river"}
-    },
-    "door": {
-        "text": "You find a mysterious door with inscriptions. Do you want to 'open', 'read', or 'leave'?",
-        "options": {"open": "exit", "read": "library", "leave": "river"}
-    },
-    "library": {
-        "text": "You're in an ancient library. Do you want to 'study', 'clue', or 'exit'?",
-        "options": {"study": "chamber", "clue": "lava", "exit": "campsite"}
-    },
-    "chamber": {
-        "text": "You discover a hidden chamber with a pedestal. Do you want to 'approach', 'look', or 'leave'?",
-        "options": {"approach": "dragon", "look": "exit", "leave": "campsite"}
-    },
-    "lava": {
-        "text": "You're at the edge of a lava pit. Do you want to 'jump', 'find', or 'retreat'?",
-        "options": {"jump": "dragon", "find": "cavern", "retreat": "library"}
-    },
-    "dragon": {
-        "text": "You've entered a dragon's lair! The dragon is asleep but stirs slightly as you approach. Do you want to 'fight' the dragon, 'sneak' past it, or 'talk' to it?",
-        "options": {"fight": "lava", "sneak": "exit", "talk": "riddle"}
-    },
-    "riddle": {
-        "text": "The dragon awakens fully and offers you a challenge. 'Answer my riddle, and I shall let you pass. Fail, and you shall return from whence you came.' The riddle is: 'What has keys but opens no locks?' Do you answer 'piano', 'chest', or 'door'?",
-        "options": {"piano": "exit", "chest": "lava", "door": "lava"}
-    },
-    "exit": {
-        "text": "You've found the exit to the cave! You see daylight streaming in. Congratulations on completing the adventure!",
-        "options": {}
-    }
+    "right_room": {
+        "text": "Oh no! It's a trap room. You are back to start. Should you 'continue'?", 
+        "options": {"continue": "start"}},
+    "treasure": {
+        "text": "Congratulations! You found the treasure!", 
+        "options": {}}
 }
-
 
 def recognize_command():
     recognizer = sr.Recognizer()
@@ -73,8 +42,6 @@ def recognize_command():
     except sr.RequestError:
         print("I felt a severed thread from a vast interconnect web. (Network Issues)")
     return ""
-
-
 
 def play_game():
     current_scene = "start"
