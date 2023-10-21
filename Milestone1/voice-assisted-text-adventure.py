e3231bd9c473d16fb481d8485cd519318e37fc45
Patrick Haynes <patrick.haynes@unomaha.edu>
2023-10-21 14:04:17 -0500

    Add more complex storyline progression

--- Milestone1/voice-assisted-text-adventure.py
+++ Milestone1/voice-assisted-text-adventure.py
@@ -3,44 +3,77 @@ import speech_recognition as sr
 # Define game's branching paths
 game = {
     "start": {
-        "text": "You are in a dark room. Do you want to go 'left' or 'right'?",
-        "options": {"left": "left_room", "right": "right_room"}
+        "text": "You find yourself at the entrance of a dimly lit cave. Do you want to 'enter' deeper into the cave or 'climb' upwards?",
+        "options": {"enter": "river", "climb": "campsite"}
     },
-    "left_room": {
-        "text": "You find a treasure chest! Do you want to 'open' it or 'leave'?",
-        "options": {"open": "treasure", "leave": "start"}
+    "river": {
+        "text": "You stumble upon an underground river. Do you want to 'swim', 'boat', or 'back'?",
+        "options": {"swim": "cavern", "boat": "door", "back": "start"}
     },
-    "right_room": {
-        "text": "Oh no! It's a trap room. You are back to start. Should you 'continue'?", 
-        "options": {"continue": "start"}},
-    "treasure": {
-        "text": "Congratulations! You found the treasure!", 
-        "options": {}}
+    "campsite": {
+        "text": "You discover an abandoned campsite. Do you want to 'search', 'ascend', or 'descend'?",
+        "options": {"search": "library", "ascend": "chamber", "descend": "start"}
+    },
+    "cavern": {
+        "text": "You're in a cavern filled with glowing crystals. Do you want to 'collect', 'explore', or 'return'?",
+        "options": {"collect": "lava", "search": "maze", "return": "river"}
+    },
+    "door": {
+        "text": "You find a mysterious door with inscriptions. Do you want to 'open', 'read', or 'leave'?",
+        "options": {"open": "library", "read": "library", "leave": "river"}
+    },
+    "library": {
+        "text": "You're in an ancient library. Do you want to 'study', 'clue', or 'exit'?",
+        "options": {"study": "chamber", "clue": "lava", "exit": "campsite"}
+    },
+    "chamber": {
+        "text": "You discover a hidden chamber with a pedestal. Do you want to 'approach', 'look', or 'leave'?",
+        "options": {"approach": "dragon", "look": "maze", "leave": "campsite"}
+    },
+    "lava": {
+        "text": "You're at the edge of a lava pit. Do you want to 'cross', 'find', or 'retreat'?",
+        "options": {"cross": "maze", "find": "cavern", "retreat": "library"}
+    },
+    "maze": {
+        "text": "You've entered a confusing maze of tunnels. Do you want to 'left', 'right', or 'straight'?",
+        "options": {"left": "chamber", "right": "lava", "straight": "dragon"}
+    },
+    "dragon": {
+        "text": "You've entered a dragon's lair! The dragon is asleep but stirs slightly as you approach. Do you want to 'fight' the dragon, 'sneak' past it, or 'talk' to it?",
+        "options": {"fight": "lava", "sneak": "exit", "talk": "riddle"}
+    },
+    "riddle": {
+        "text": "The dragon awakens fully and offers you a challenge. 'Answer my riddle, and I shall let you pass. Fail, and you shall return from whence you came.' The riddle is: 'What has keys but opens no locks?' Do you answer 'piano', 'chest', or 'door'?",
+        "options": {"piano": "exit", "chest": "lava", "door": "lava"}
+    },
+    "exit": {
+        "text": "You've found the exit to the cave! You see daylight streaming in. Congratulations on completing the adventure!",
+        "options": {}
+    }
 }
 
-def recognize_command():
+def recognize_command(retries=3):
     recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-    
-    with microphone as source:
-        print("Say something...")
-        recognizer.adjust_for_ambient_noise(source, duration=2)  # Adjusting for ambient noise
+
+    for _ in range(retries):
         try:
-            audio = recognizer.listen(source, timeout=5)  # 5 seconds timeout
-            recognizer.energy_threshold += 500  # Increase the energy threshold
+            with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source, duration=2)  # Adjusting for ambient noise once
+                print("Say something...")
+                audio = recognizer.listen(source, timeout=10)  # Increase timeout to 10 seconds
+                command = recognizer.recognize_google(audio)
+                print("You said:", command)
+                return command.lower()
         except sr.WaitTimeoutError:
             print("You didn't say anything. Please try again.")
-            return None
-        
-    try:
-        command = recognizer.recognize_google(audio)
-        print("You said:", command)
-        return command.lower()
-    except sr.UnknownValueError:
-        print("You mumbled something unrecognizable. Please try again.")
-        return None
-    except sr.RequestError:
-        print("I felt a severed thread from a vast interconnect web. (Network Issues)")
+        except sr.UnknownValueError:
+            print("You mumbled something unrecognizable. Please try again.")
+        except sr.RequestError:
+            print("I felt a severed thread from a vast interconnect web. (Network Issues)")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}. Please try again.")
+
+    print("Sorry, we're having trouble understanding you. Please try again.")
     return ""
 
 def play_game():
